5555fe15c8d2fd6c8cf2c8ec57ca26d1077f1e3e
Michael Maurizi <mmaurizi@azavea.com>
2016-02-05 14:29:40 -0500

    Add basic Windshaft tile server for serving census blocks

---
+++ tiler.js
@@ -0,0 +1,53 @@
+var Windshaft = require('windshaft'),
+    fs = require('fs'),
+    styles = fs.readFileSync('styles.mss', { encoding: 'utf8' });
+
+var dbUser = process.env.DB_USER || 'access',
+    dbPass = process.env.DB_PASSWORD || 'access',
+    dbHost = process.env.DB_HOST || 'localhost',
+    dbName = process.env.DB_NAME || 'access',
+    dbPort = process.env.DB_PORT || '5432',
+    redisHost = process.env.REDIS_HOST || 'localhost',
+    redisPort = process.env.REDIS_PORT || '6379';
+
+var config = {
+    base_url: '/tiles',
+    base_url_notable: '/tiles',
+    grainstore: {
+        datasource: {
+            user: dbUser,
+            host: dbHost,
+            port: dbPort,
+            password: dbPass,
+            geometry_field: 'geom_wm',
+            srid: 3857
+        }
+    },
+    renderCache: {
+        ttl: 60000, // seconds
+    },
+    mapnik: {
+        metatile: 4,
+        bufferSize: 64,
+    },
+    redis: {
+        host: redisHost,
+        port: redisPort
+    },
+    req2params: function(req, callback) {
+        try {
+            req.params.table = 'dest_blocks';
+            req.params.dbname = dbName;
+            req.params.style = styles;
+            req.params.interactivity = 'geoid10';
+            callback(null, req);
+        } catch(err) {
+            callback(err, null);
+        }
+    }
+};
+
+// Initialize tile server on port 4000
+var ws = new Windshaft.Server(config);
+ws.listen(4000);
+console.log('Starting Windshaft tiler on http://localhost:4000' + config.base_url + '/:z/:x/:y.*');
