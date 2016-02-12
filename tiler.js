c73f8fdc04f2b9f677c42ba17af136cd9a3b0e96
Kathryn Killebrew <banderkat@gmail.com>
2016-02-12 00:03:45 -0500

    Style block tiles by metric

--- tiler.js
+++ tiler.js
@@ -1,17 +1,34 @@
 var Windshaft = require('windshaft'),
-    fs = require('fs'),
-    styles = fs.readFileSync('styles.mss', { encoding: 'utf8' });
+    fs = require('fs');
 
-var dbUser = process.env.DB_USER || 'access',
-    dbPass = process.env.DB_PASSWORD || 'access',
+var dbUser = process.env.DB_USER || 'transit_access',
+    dbPass = process.env.DB_PASSWORD || 'transit_access',
     dbHost = process.env.DB_HOST || 'localhost',
     dbName = process.env.DB_NAME || 'access',
     dbPort = process.env.DB_PORT || '5432',
     redisHost = process.env.REDIS_HOST || 'localhost',
     redisPort = process.env.REDIS_PORT || '6379';
 
+function getStyle(metric) {
+    var baeStyleStart = '#philly_block_access {';
+    var baseStyleRest = [
+        'polygon-opacity: 0.25;',
+        'line-opacity: 0.4;',
+    '}'].join('');
+
+    var blues = ['#ffffff','#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'];
+
+    var style = baeStyleStart;
+    i = 0;
+    for (i = 0; i < blues.length; i++) {
+        style += '[' +  metric + ' >= ' + (i * 10) + '] { polygon-fill: ' + blues[i] + '; line-color: ' + blues[i] + '}';
+    }
+
+    return style + baseStyleRest;
+}
+
 var config = {
-    base_url: '/tiles',
+    base_url: '/tiles/:metric',
     base_url_notable: '/tiles',
     grainstore: {
         datasource: {
@@ -34,11 +51,12 @@ var config = {
         host: redisHost,
         port: redisPort
     },
+    enable_cors: true,
     req2params: function(req, callback) {
         try {
-            req.params.table = 'dest_blocks';
+            req.params.table = 'philly_block_access';
             req.params.dbname = dbName;
-            req.params.style = styles;
+            req.params.style = getStyle(req.params.metric);
             req.params.interactivity = 'geoid10';
             callback(null, req);
         } catch(err) {
